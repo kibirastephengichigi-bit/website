c7c2bd912ace44ac69550a4a3f3b5a6aede13d8e
Cyberverse-cent0 <johnsonkopeng001@gmail.com>
2026-04-24 14:35:14 +0300

    Fix JSX syntax error in admin signup page - add missing closing brace

--- app/admin-signup/page.tsx
+++ app/admin-signup/page.tsx
@@ -220,6 +220,7 @@ export default function AdminSignInPage() {
                         </button>
                       </div>
                     </div>
+                  </div>
 
                   {/* Submit Button */}
                   <Button
